388b4a9448edd71e355ccc60095d9126ca374b32
Javonte Tuppince <taetupp47@gmail.com>
2021-02-01 16:25:07 -0500

    made a dummy page for testi

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Signup from "./pages/signup";
 import Detail from "./pages/Detail";
 import Testimonials from "./pages/Testimonials";
 import  NavBar  from "./components/NavBar";
-import  NavLink  from "./components/Navlink";
+//import  NavLink  from "./components/Navlink";
 
 function App() {
   return (
