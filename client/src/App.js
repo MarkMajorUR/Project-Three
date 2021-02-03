0b5ede3eeba2dc64c6cea4f8ebaee8e60ff9b20a
Mark Major <mmajor@valcv.org>
2021-02-02 19:13:09 -0500

    node modules?? maynbe?? and fixed app.js according to GIO slack

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Goals from "./pages/Goals";
-<<<<<<< HEAD
 import Signup from "./pages/signup"
-=======
 import Detail from "./pages/Detail";
->>>>>>> 5c21467fc7b064cf17c700e77c69338d50e4ce5b
 
 function App() {
   return (
@@ -13,23 +10,22 @@ function App() {
       <div>
         <Switch>
           <Route exact path={["/", "/goals"]}>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
             <Goals />
-=======
-            <Books />
->>>>>>> 31f839dec00cea22d827eb00dbd5bf9160c06d7e
+
+         
+
           </Route>
           <Route exact path="/goals/:id">
            
-=======
+
             <Goals />
           </Route>
           <Route exact path="/goals/:id">
             <Detail />
->>>>>>> 5c21467fc7b064cf17c700e77c69338d50e4ce5b
+
           </Route>
-          <Route exact path={["/Signup"]}>
+          <Route exact path={["/", "/register"]}>
             <Signup />
           </Route>
           <Route>
